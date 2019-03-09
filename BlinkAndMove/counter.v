eddab8870296751591d93fc1c61f20b2916652c5
maximdorogov <maxim_dorogov@yahoo.com>
2019-03-09 18:20:23 -0300

    actualizo fuentes

--- BlinkAndMove/counter.v
+++ BlinkAndMove/counter.v
@@ -3,54 +3,7 @@ module  counter(
    			 input i_rst, 
    			 input i_enable,
    			 input [1:0] i_sel,
-   			 output reg o_valid,    				 
+   			 output  o_valid    				 
   );
-	
-	localparam 1000MS = 32'd100_000_000;
-	localparam 500MS = 32'd50_000_000;
-	localparam 250MS = 32'd25_500_000;
-	localparam 100MS = 32'd12_000_000;
-	
-	reg [31:0]compare;
-	reg [31:0]counter;
-
-	always @(posedge clk_in) begin
-	
-		case(i_sel)
-				2'b00: compare = 1000MS;
-				2'b01: compare = 500MS;
-				2'b10: compare = 250MS;
-				2'b11: compare = 100MS;
-		endcase
-		
-	    if (i_rst) begin
-	     
-	      counter <= 32'd0;
-	      compare <=  1000MS;
-	      o_valid <= 1'b0; 
-	          
-	    end else begin
-	    
-	    if (o_valid) begin
-
-	    	counter <= counter + 32'b1;
-
-		    if(counter >= compare) begin
-		          
-		        counter <= 0;
-		        o_valid <= 1; 
-		          
-		      end else begin
-		      	o_valid <= 1'b0;
-		      end   
-		 	end
-	    end else begin
-	    	counter <= counter;
-	    end 
-
-	end
-
-	
-
 
 endmodule
--- BlinkAndMove/top_level_leds.v
+++ BlinkAndMove/top_level_leds.v
@@ -10,21 +10,19 @@ module top_level_leds /*#(parameter F_CLK_OUT = 1, F_CLK_IN = 50_000_000)*/ (
     output [3:0] o_ledG
   );
 
-  wire valid;
-
   counter  u_counter(
   			   .clk(clk),
    			   .i_rst(i_rst), 
-   			   .i_enable(i_sw[0]),
-   			   .i_sel(i_sw[2:1]),
-   			   .o_valid(valid)     				 
+   			   .i_enable(),
+   			   .i_sel(),
+   			   .o_valid()     				 
   );
 
   shift_reg u_shift_reg(
 
-  				.i_rst(i_rst),
-  				.clk(clk),
-  				.i_valid(valid),
+  				.rst(),
+  				.clk(),
+  				.i_valid(),
   				.o_data()
   				);
 endmodule
