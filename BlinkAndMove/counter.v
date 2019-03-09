29e307be94a14be86ae96b0d8fcf9181d7bb2d0d
maximdorogov <maxim_dorogov@yahoo.com>
2019-03-09 12:51:19 -0300

    agrego modulos shift_reg y counter

---
+++ BlinkAndMove/counter.v
@@ -0,0 +1,9 @@
+module  counter(
+  			 input clk,
+   			 input i_rst, 
+   			 input i_enable,
+   			 input [1:0] i_sel,
+   			 output  o_valid    				 
+  );
+
+endmodule
---
+++ BlinkAndMove/shift_reg.v
@@ -0,0 +1,8 @@
+module  shift_reg(
+  			 input clk,
+   			 input i_rst, 
+   			 input  i_valid,
+   			 output o_data    				 
+  );
+
+endmodule
