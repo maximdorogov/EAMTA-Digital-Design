d164305c3e43a94d42ef5b11f5b518c1ff78bbce
maximdorogov <maxim_dorogov@yahoo.com>
2019-03-09 18:58:14 -0300

    Inicializo repo

--- BlinkAndMove/top_level_leds.v
+++ BlinkAndMove/top_level_leds.v
@@ -10,19 +10,26 @@ module top_level_leds /*#(parameter F_CLK_OUT = 1, F_CLK_IN = 50_000_000)*/ (
     output [3:0] o_ledG
   );
 
+  wire valid;
+  wire [3:0] dataLed;
+
+  assign o_led = dataLed;
+
+  assign o_ledR = (i_sw[3])?dataLed:4'd0;
+
   counter  u_counter(
   			   .clk(clk),
    			   .i_rst(i_rst), 
-   			   .i_enable(),
-   			   .i_sel(),
-   			   .o_valid()     				 
+   			   .i_enable(i_sw[0]),
+   			   .i_sel(i_sw[2:1]),
+   			   .o_valid(valid)     				 
   );
 
   shift_reg u_shift_reg(
 
-  				.rst(),
-  				.clk(),
-  				.i_valid(),
-  				.o_data()
+  				.i_rst(i_rst),
+  				.clk(clk),
+  				.i_valid(valid),
+  				.o_data(dataLed)
   				);
 endmodule
---
+++ BlinkAndMove/top_level_leds_tb.v
@@ -0,0 +1,23 @@
+
+`define N_LEDS 4
+`define NB_SW 4
+
+module top_level_leds_tb (   
+   /*no hay salidas ni entradas*/   
+  );
+
+	parameter N_LEDS = `N_LEDS;
+
+	wire [N_LEDS - 1:0] o_led;
+	wire [NB_SW - 1:0] i_sw;
+	reg ck_rst;
+	reg CLK100MHZ;
+
+
+  initial begin
+    clk = 1'b0;
+    rst = 1'b1;
+    repeat(4) #20 clk = ~clk;    //1ns es una ud de tiempo para el Isim. 
+    rst = 1'b0;
+    forever #20 clk = ~clk; // generate a clocK 
+  end
