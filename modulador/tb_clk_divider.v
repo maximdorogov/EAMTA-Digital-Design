14da5a10d045066d215ab5cfa634042d8b800ba4
maximdorogov <maxim_dorogov@yahoo.com>
2019-03-12 00:41:00 -0300

    dejo en modulador solo los archivos funcionales

---
+++ modulador/tb_clk_divider.v
@@ -0,0 +1,28 @@
+// testbench para el modulo counter implementardo en la 1er clase EAMTA2019
+
+
+module tb_counter (   
+   /*no hay salidas ni entradas*/   
+  );
+
+  reg clk, rst; 
+  wire clk_out;
+
+  initial begin
+    clk = 1'b0;
+    rst = 1'b1;
+    repeat(4) #20 clk = ~clk;    //1ns es una ud de tiempo para el Isim. 
+    rst = 1'b0;
+    forever #20 clk = ~clk; // generate a clocK 
+  end
+
+clk_divider #(.NBITS(3)) DUT (
+
+		.i_clk(clk),  // clock
+    	.i_rst(rst),  // reset
+    	.o_clk(clk_out)	
+
+);
+
+
+endmodule
--- modulador/top.v
+++ modulador/top.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module top #(parameter NB_COUNT = 3)( 
 
 			input i_reset,
@@ -45,52 +44,5 @@ assign valid = (count == {NB_COUNT{1'b1}})?1'b1:1'b0;
 			   .i_prbs(data_prbs)
 			  // o_data();
 		);
-=======
-module top #(parameter NB_COUNT = 3)( 
 
-			input i_reset,
-			input [3:0] i_enable,
-			input clock,
-			output [3:0] o_leds
-			);
-
-reg [NB_COUNT-1:0] count;
-wire valid;
-wire data_prbs;
-
-assign o_leds = i_enable;
-
-
-always@(posedge clock) begin
-		
-		if (!i_reset) 
-			
-			count <= {NB_COUNT{1'b0}};
-
-		else 
-			if (i_enable[0])
-				count <= count + 1;
-			else 
-				count <= count;
-end
-
-assign valid = (count == {NB_COUNT{1'b1}})?1'b1:1'b0;
-
-	PRBS
-		u_prbs
-		   (.o_data(data_prbs),
-			.i_reset(~i_reset),
-			.i_valid(valid),
-			.clock(clock),
-			.i_enable(i_enable[1]));
-		
-	fir_filter
-		u_fir( .clk(clock),
-			   .i_rst(~i_reset),
-			   .i_valid(valid),
-			   .i_enable(i_enable[2]),
-			   .i_prbs(data_prbs)
-			  // o_data();
-		);
->>>>>>> 44ed12a16dbc8675bc081c561baf0f54c9228993
 endmodule
